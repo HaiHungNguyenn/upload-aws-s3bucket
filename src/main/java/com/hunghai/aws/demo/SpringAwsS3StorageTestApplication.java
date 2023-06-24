45b43c302cdfc319be08a03ab07d9df2060351d3
HaiHungNguyenn <hainhse173100@fpt.edu.vn>
2023-06-24 20:17:19 +0700

    upload/delete/ generate url file

---
+++ src/main/java/com/hunghai/aws/demo/SpringAwsS3StorageTestApplication.java
@@ -0,0 +1,13 @@
+package com.hunghai.aws.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsS3StorageTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAwsS3StorageTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hunghai/aws/demo/SpringAwsS3StorageTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunghai.aws.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsS3StorageTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
